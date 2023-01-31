8fc1263b4557d7f7b9e4f2d67ed833a190737a06
roryobrien03 <122220434+roryobrien03@users.noreply.github.com>
2023-01-31 16:47:58 +0000

    Delete datasetup.py

--- datasetup.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("pl1819.csv")
-df2 = df[['home_team_name', 'away_team_name', 'home_team_goal_count', 'away_team_goal_count', 'odds_ft_home_team_win', 'odds_ft_draw', 'odds_ft_away_team_win']]
